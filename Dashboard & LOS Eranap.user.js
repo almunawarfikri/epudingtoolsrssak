2256045e317e56b4eab94d114999a4d1f10c7599
Fikri Al-Munawar <almunawarfikri@gmail.com>
2026-07-08 11:57:05 +0700

    Bump version to 3.1

--- Dashboard & LOS Eranap.user.js
+++ Dashboard & LOS Eranap.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dashboard & LOS Eranap (Combined) RS SA Karawaci
 // @namespace    http://tampermonkey.net/
-// @version      3.0
+// @version      3.1
 // @description  Dashboard TKMKB + LOS RS + LOS BPJS + Auto 200 + CPPT Diagnosa Viewer
 // @author       Fikri
 // @updateURL    https://raw.githubusercontent.com/almunawarfikri/toolsrssak/main/Dashboard%20&%20LOS%20Eranap.obfuscated.user.js
